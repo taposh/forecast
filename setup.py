d222405c7cf5857b90b1004c0272cc78af5a7166
Avnish Narayan <avnishna@usc.edu>
2019-07-01 13:51:08 -0700

    Restructuring for easier install

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
+from setuptools import find_packages
 from setuptools import setup
 
-setup(name='forecast',
+required = [
+      "forecast",
+      "pandas",
+      "python-dateutil",
+      "numpy",
+      "sklearn",
+      "torch"
+]
+
+setup(name='kpforecast',
       version='0.0.2',
-      description='The forecast package',
+      description='The forecast package used by kp insight',
       url='https://github.com/taposh/forecast.git',
-      author='Taposh Roy',
-      author_email='taposh@arithmetica.io',
+      author='KP-Insight',
+      author_email='taposh.d.roy@kp.org',
       license='MIT',
-      packages=['forecast'],
+      packages=find_packages(where='src'),
+      package_dir={'': 'src'},
+      install_requires=required,
       zip_safe=False)
