66c6b9fdbfe43bf92d55f71812809c4593f77876
Avnish Narayan <avnish.x.narayan@kp.org>
2019-08-12 17:59:22 -0700

    Add Seasonality and Trend Blocks
    - Add functioning Seasonality and Trend blocks
      for reducing model size

--- src/kpforecast/ml/n_beats/blocks/seasonality_block.py
+++ src/kpforecast/ml/n_beats/blocks/seasonality_block.py
@@ -1,15 +1,56 @@
 from kpforecast.ml.n_beats.blocks import NBeatsBlock
 
+import numpy as np
 import torch
 import torch.nn as nn
 
-class SeasonalityBlock(nn.Module):
+class SeasonalityBlock(NBeatsBlock):
+    """ Seasonality N_Beats Block.
+    Args:
+        -f_b_dim(list/tuple): The integer length of the
+             forward and backwards forecast
+        -thetas_dim(list/tuple): list or iterable of output
+        dimensions of the theta output layers.
+        (None by default, results in thetas dim being
+        a list same where entries are same as hidden_layer_dim)
+        -hidden_layer_dim(int): dimension of input and outputs of
+            input and hidden layers (1 by default)
+        -num_hidden_layers(int): number of hidden layers
+            (2 by default)
+        -layer_nonlinearity: torch.nn nonlinearity function
+            to use (ReLU by default)
+        -layer_w_init: torch.nn.init function to use to
+            initialize weight vars 
+            (xavier uniform by default)
+        -layer_b_init: torch.nn.init function to use to 
+            initialize bias constants (zeros by default)
+    """
+    def __init__(self,
+                 f_b_dim,
+                 thetas_dim=None,
+                 num_hidden_layers=2,
+                 hidden_layer_dim=1,
+                 layer_nonlinearity=nn.ReLU,
+                 layer_w_init=nn.init.xavier_uniform_,
+                 layer_b_init=nn.init.zeros_):
 
-    def __init__(self):
-        pass
+        super(SeasonalityBlock, self).__init__(
+                    f_b_dim=f_b_dim,
+                    thetas_dim=thetas_dim,
+                    num_hidden_layers=num_hidden_layers,
+                    hidden_layer_dim=hidden_layer_dim,
+                    layer_nonlinearity=layer_nonlinearity,
+                    layer_w_init=layer_w_init,
+                    layer_b_init=layer_b_init)
+        self.f_b_dim = f_b_dim
+        self.backcast_ls, self.forecast_ls = self.linspace()
 
     def forward(self, input_val):
-        pass
+        import ipdb; ipdb.set_trace()
+        thetas = super(SeasonalityBlock, self).forward(input_val)
+        backcast = self.seasonality_model(thetas[1], self.backcast_ls)
+        forecast = self.seasonality_model(thetas[0], self.forecast_ls)
+        return forecast, backcast
 
     def seasonality_model(self, thetas, t):
         p = thetas.size()[-1]
@@ -19,3 +60,11 @@ class SeasonalityBlock(nn.Module):
         s2 = torch.tensor([np.sin(2 * np.pi * i * t) for i in range(p2)]).float()
         S = torch.cat([s1, s2])
         return thetas.mm(S)
+
+    def linspace(self):
+        backcast_length = self.f_b_dim[1]
+        forecast_length = self.f_b_dim[0]
+        lin_space = np.linspace(-backcast_length, forecast_length, backcast_length + forecast_length)
+        b_ls = lin_space[:backcast_length]
+        f_ls = lin_space[backcast_length:]
+        return b_ls, f_ls
--- src/kpforecast/ml/n_beats/blocks/trend_block.py
+++ src/kpforecast/ml/n_beats/blocks/trend_block.py
@@ -1,20 +1,75 @@
 
 from kpforecast.ml.n_beats.blocks import NBeatsBlock
 
+import numpy as np
 import torch
 import torch.nn as nn
 
-class TrendBlock(nn.Module):
+class TrendBlock(NBeatsBlock):
+    """ Trend N_Beats Block.
+    Args:
+        -f_b_dim(list/tuple): The integer length of the
+             forward and backwards forecast
+        -thetas_dim(list/tuple): list or iterable of output
+        dimensions of the theta output layers.
+        (None by default, results in thetas dim being
+        a list same where entries are same as hidden_layer_dim)
+        -hidden_layer_dim(int): dimension of input and outputs of
+            input and hidden layers (1 by default)
+        -num_hidden_layers(int): number of hidden layers
+            (2 by default)
+        -layer_nonlinearity: torch.nn nonlinearity function
+            to use (ReLU by default)
+        -layer_w_init: torch.nn.init function to use to
+            initialize weight vars 
+            (xavier uniform by default)
+        -layer_b_init: torch.nn.init function to use to 
+            initialize bias constants (zeros by default)
+    """
+    def __init__(self,
+                 f_b_dim,
+                 thetas_dim=None,
+                 num_hidden_layers=2,
+                 hidden_layer_dim=1,
+                 layer_nonlinearity=nn.ReLU,
+                 layer_w_init=nn.init.xavier_uniform_,
+                 layer_b_init=nn.init.zeros_):
 
-    def __init__(self):
-        pass
+        super(TrendBlock, self).__init__(
+                    f_b_dim=f_b_dim,
+                    thetas_dim=thetas_dim,
+                    num_hidden_layers=num_hidden_layers,
+                    hidden_layer_dim=hidden_layer_dim,
+                    layer_nonlinearity=layer_nonlinearity,
+                    layer_w_init=layer_w_init,
+                    layer_b_init=layer_b_init)
+        self.f_b_dim = f_b_dim
+        self.backcast_ls, self.forecast_ls = self.linspace()
 
     def forward(self, input_val):
-        pass
+        """ Feed Forward function for TrendBlock module.
 
+        Args:
+            input val(torch.tensor): input for block
+        Returns:
+            List of torch.tensors that are outputs of the network,
+            Where each output is from a different output head
+        """
+        thetas = super(TrendBlock, self).forward(input_val)
+        backcast = self.trend_model(thetas[1], self.backcast_ls)
+        forecast = self.trend_model(thetas[0], self.forecast_ls)
+        return forecast, backcast
 
     def trend_model(self, thetas, t):
         p = thetas.size()[-1]
         assert p <= 4, 'thetas_dim is too big.'
         T = torch.tensor([t ** i for i in range(p)]).float()
         return thetas.mm(T)
+
+    def linspace(self):
+        backcast_length = self.f_b_dim[1]
+        forecast_length = self.f_b_dim[0]
+        lin_space = np.linspace(-backcast_length, forecast_length, backcast_length + forecast_length)
+        b_ls = lin_space[:backcast_length]
+        f_ls = lin_space[backcast_length:]
+        return b_ls, f_ls
